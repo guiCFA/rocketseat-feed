304ffbdd223a1c8b68fee85e3cb3fa73e860d292
guiCFA <guilhermecriferreira@gmail.com>
2022-07-15 18:26:41 -0300

    adicionando projeto

---
+++ src/components/Comment.jsx
@@ -0,0 +1,48 @@
+import { Trash, ThumbsUp } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment({content, onDeleteComment}) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment(){
+    onDeleteComment(content); 
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/guiCFA.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong> Guilherme Araújo </strong>
+              <time title='12 de Julho às 01:28h' dateTime='2022-12-07 01:28:55'> Cerca de 1h atrás </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          
+          <p>{ content }</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp size={20} /> Aplaudir <span> { likeCount } </span>
+          </button>
+        </footer>
+      </div>
+
+    </div>
+  );
+}
